42bdbb44bb1a85a07a9dd1d637c1715ff3f90ab6
Walter Schweitzer <wschweitzer00@gmail.com>
2017-02-10 01:47:31 +0000

    app.js updates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.send('test')
 })
@@ -10,6 +12,10 @@ app.get('/test', (req, res) => {
 })
 
 app.listen(3000, () => {
-     console.log('Listening on 80')
+    console.log('Listening on 80')
+})
+
+app.get('/node-js', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
